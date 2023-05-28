32a8f9e67d622d98f0f3b977e687468a2ac1c199
Clément Petit <clement.petit22@gmail.com>
2023-05-28 15:19:09 +0200

    Optimise imports

--- src/test/kotlin/au/com/console/jpaspecificationdsl/JPASpecificationDSLTest.kt
+++ src/test/kotlin/au/com/console/jpaspecificationdsl/JPASpecificationDSLTest.kt
@@ -5,14 +5,12 @@ import org.hamcrest.Matchers
 import org.hamcrest.Matchers.containsInAnyOrder
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.jpa.domain.Specification
 import org.springframework.transaction.annotation.Transactional
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootApplication
 open class TestApplication
