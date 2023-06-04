a98cf0211c492f2335f360e12ae4bdd355024149
Clément Petit <clement.petit22@gmail.com>
2023-06-04 14:42:22 +0200

    Rename jpa module cont\'d

--- src/jpadsl/src/main/kotlin/io/github/petitcl/springdata/jpasdsl/JPASpecificationDSL.kt
+++ src/jpadsl/src/main/kotlin/io/github/petitcl/springdata/jpasdsl/JPASpecificationDSL.kt
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.domain.Specification
 import javax.persistence.criteria.*
 import kotlin.reflect.KProperty1
 
+
+
 // Helper to allow joining to Properties
 fun <Z, T, R> From<Z, T>.join(prop: KProperty1<T, R?>): Join<T, R> = this.join<T, R>(prop.name)
 
